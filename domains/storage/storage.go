ccc9645d038442e704b88076276ecb0437f74fcb
Alugbin Abiodun Olutola <alugbin.abiodun@gmail.com>
2023-04-25 21:45:24 +0200

    add comment on exported interface storer

--- domains/storage/storage.go
+++ domains/storage/storage.go
@@ -2,6 +2,7 @@ package storage
 
 import "key-value-ts/domains/entities"
 
+// Storer interface that defines the basic logic for any storage implementation
 type Storer interface {
 	Save(e entities.Sequence) error
 	Get(key string, timestamp int64) (string, error)
